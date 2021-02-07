df12606a6b0795dee115f1d2777b4ac3b73d4b59
kplam624 <kplam624@gmail.com>
2021-02-06 21:16:31 -0500

    Cleaned up the code.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,41 +3,51 @@
 // Colors the circles based on depth.
 function circleColor(depth){
   if (depth < 10){
-    return "#003300";
+    return "#99ff99";
   }
   else if (depth < 30){
-    return "#009900"
+    return "#66ff66"
   }
   else if (depth < 50){
-    return "#99ff33"
+    return "#33cc33"
   }
   else if (depth < 70){
-    return "#ff0000"
+    return "#009933"
   }
   else{
-    return "#800000";
+    return "#003300";
   };
 };
 
+// Creates the map for the leaflet
 var myMap = L.map("map",{
     center: [38.8003, -102.6216],
     zoom: 3
 });
 
+// Creates the legend and positions it.
 var legend = L.control({position: "bottomright"});
 
+// The legend itself.
 legend.onAdd = function (myMap) {
+  
+  // Creates a div called .info.legend.
   var div = L.DomUtil.create('div', 'info legend'),
+  
+  // Creates the bins.
   grades = [-10, 10, 30, 50, 70],
   labels = [];
   
-  var legends = "<h5> Earthquake Magnitudes </h5>";
+  // Creates the title
+  var legendTitle = "<h5> Earthquake Magnitudes </h5>";
 
-  div.innerHTML = legends;
+  // Adds the title to the div.
+  div.innerHTML = legendTitle;
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
 
+      // Will add the rest of the legend to the div
       div.innerHTML +=
           '<i style="background-color: ' + circleColor(grades[i] + 1) + '"></i> ' +
           grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
@@ -45,6 +55,7 @@ legend.onAdd = function (myMap) {
   return div;
 };
 
+// Adds the legend to the map.
 legend.addTo(myMap);
 
 // Tile layer for the map.
@@ -67,21 +78,24 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geo
   for (var i = 0; i < geoFeatures.length; i++){
     var earthquake = geoFeatures[i];
 
+    // Creates variables that hold properties from the geojson
     var latlng = [earthquake.geometry.coordinates[1],earthquake.geometry.coordinates[0]];
     var depth = earthquake.geometry.coordinates[2];
     var mag = earthquake.properties.mag;
     var location = earthquake.properties.place;
 
+    // Creates the markers for each earthquake.
     L.circleMarker(latlng,{
       color: "black",
       fillColor: circleColor(depth),
       fillOpacity: 1,
       radius: mag * 3,
+      // Binds a pop up to the markers.
     }).bindPopup(`<strong>Location:</strong> ${location} <br> <strong>Magnitude:</strong> ${mag} 
     <br> <strong>Depth: </strong> ${depth} km`)
       .addTo(myMap)
   };
-  // searches the geojson for the coordinates
-  console.log("Eyy");
-
+  
+  // shows if the code had finished running.
+  console.log("done");
 });
